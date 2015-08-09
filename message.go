b306b8d026c912fff15667b2b0ed488090784208
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-10 00:23:42 +0100

    wrapping time to show it properly

--- message.go
+++ message.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
+
+// Marshaler implements Unmarshaler interface
+type Marshaler interface {
+	MarshalJSON() ([]byte, error)
+}
+
+// JSONTime formatting time
+type JSONTime time.Time
+
+// MarshalJSON wraps time.Time to return formated version
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	//do your serializing here
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("Mon Jan _2 15:04:05"))
+	return []byte(stamp), nil
+}
 
 // message represents a single msg
 // now attaching sender's name, message and time
 type message struct {
 	Name    string
 	Message string
-	When    time.Time
+	When    JSONTime
 }
