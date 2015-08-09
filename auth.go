d19126f5ce6ba05c67b570bb327f5d28537bf478
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-09 17:12:56 +0100

    added login handler

--- auth.go
+++ auth.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
 )
 
 type authHandler struct {
@@ -23,4 +26,21 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 func MustAuth(handler http.Handler) http.Handler {
 	return &authHandler{next: handler}
+}
+
+// loginHandler handles the third-party login process.
+// format: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	// TODO: handle exception if there are too few segments
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO handle login for", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "auth action %s not supported", action)
+
+	}
 }
