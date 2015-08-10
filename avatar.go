0c3e3f5edff636b18ffd5e8a5cc498405fc36b3c
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-10 11:37:57 +0100

    avatar abstraction

--- avatar.go
+++ avatar.go
@@ -1,6 +1,12 @@
 package main
 
+import (
+	"errors"
+)
+
 // ErrNoAvatar is the error when no avatar URL is available
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL")
+
 // Avatar represents types capable of representing
 // user profile pictures.
 type Avatar interface {
@@ -8,4 +14,20 @@ type Avatar interface {
 	// ErrNoAvatarURL is returned if the object is unable to get url for specified
 	// client
 	GetAvatarURL(c *client) (string, error)
+}
+
+// AuthAvatar structure, zero init
+type AuthAvatar struct{}
+
+// UseAuthAvatar initialises AuthAvatar struct
+var UseAuthAvatar AuthAvatar
+
+// GetAvatarURL gets user's avatar or return an error
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
 }
