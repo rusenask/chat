6f136553724f91bf233babc21946c5affb2e6d52
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-10 10:00:30 +0100

    passing avatar from cookie to message

--- client.go
+++ client.go
@@ -24,6 +24,10 @@ func (c *client) read() {
 		if err := c.socket.ReadJSON(&msg); err == nil {
 			msg.When = JSONTime(time.Now())
 			msg.Name = c.userData["name"].(string)
+			// avatarUrl is assigned from cookie
+			if avatarURL, ok := c.userData["avatar_url"]; ok {
+				msg.AvatarURL = avatarURL.(string)
+			}
 			c.room.forward <- msg
 		} else {
 			break
