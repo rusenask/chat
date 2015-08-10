45420aa84075d54b8d6635581229333baf527ec4
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-10 17:34:09 +0100

    tests for gravatar

--- avatar_test.go
+++ avatar_test.go
@@ -23,3 +23,16 @@ func TestAuthAvatar(t *testing.T) {
 		}
 	}
 }
+
+func TestGravatarAvatar(t *testing.T) {
+	var gravatarAvitar GravatarAvatar
+	client := new(client)
+	client.userData = map[string]interface{}{"email": "MyEmailAddress@example.com"}
+	url, err := gravatarAvitar.GetAvatarURL(client)
+	if err != nil {
+		t.Error("GravatarAvitar.GetAvatarURL should not return an error")
+	}
+	if url != "//www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346" {
+		t.Errorf("GravatarAvitar.GetAvatarURL wrongly returned %s", url)
+	}
+}
