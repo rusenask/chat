e039d2350e02f4e0f78ad727ebbe6dc3ec8585a7
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-11 11:48:33 +0100

    cleanup

--- room.go
+++ room.go
@@ -26,14 +26,13 @@ type room struct {
 }
 
 // newRom makes a new room that is ready to go.
-func newRoom(avatar Avatar) *room {
+func newRoom() *room {
 	return &room{
 		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
 		tracer:  trace.Off(),
-		avatar:  avatar,
 	}
 }
 
